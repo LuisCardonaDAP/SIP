3ccfeb0b43e2610aea01c3de22a4cbaf4382e6bc
José Cardona <josecardonariv117@gmail.com>
2026-01-07 20:11:42 +0000

    Hay un pequeño error en la vista, no abarca la pantalla completa, aún un

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
 
   return (
     <header className="border-b bg-card sticky top-0 z-20">
-      <div className="container mx-auto flex items-center justify-between px-4 py-2">
+      <div className="flex items-center justify-between px-4 py-2">
         <div className="flex items-center gap-2">
            <SidebarTrigger className="md:hidden"/>
            <Logo />
